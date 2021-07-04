bb646d9b8b61fb7752042904404af0352d11a633
SparshKhanna0001 <65919066+SparshKhanna0001@users.noreply.github.com>
2021-07-04 20:27:28 +0530

    Made data when demanded more readable.

--- si.py
+++ si.py
@@ -6,22 +6,20 @@ conn = sqlite3.connect("tasks.db")
 cur = conn.cursor()
 
 type_of_opr = sys.argv[1] 
-schema = "(Taskname varchar, type of task; long/short, priority, Date of entry, week num, date of completion, description)"
-print(schema)
 
 if type_of_opr.lower() == "all":
   
   for row in cur.execute("SELECT * FROM tasks"):
-    print(row)
+    print(f"""(SerialNum = {row[0]}, taskname={row[1]},type={row[2]},Subject={row[3]}, priorityType={row[4]}, description={row[5]},Status={row[6]},DateOfEntry = {row[7]}, weeknum={row[8]},DateCompletion={row[9]},estimatedType = {row[10]})""")
 
 elif type_of_opr.lower() == "pen":
   
   for row in cur.execute("SELECT * FROM tasks WHERE Status == 'pending'"):
-    print(row)
+    print(f"""(SerialNum = {row[0]}, taskname={row[1]},type={row[2]},Subject={row[3]}, priorityType={row[4]}, description={row[5]},Status={row[6]},DateOfEntry = {row[7]}, weeknum={row[8]},DateCompletion={row[9]},estimatedType = {row[10]})""")
     
 elif type_of_opr.lower() == "done":
   
   for row in cur.execute("SELECT * FROM tasks WHERE Status == 'done'"):
-    print(row)
+    print(f"""(SerialNum = {row[0]}, taskname={row[1]},type={row[2]},Subject={row[3]}, priorityType={row[4]}, description={row[5]},Status={row[6]},DateOfEntry = {row[7]}, weeknum={row[8]},DateCompletion={row[9]},estimatedType = {row[10]})""")
   
 conn.commit()
