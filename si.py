45747649b7df23df6acc82cf20204e3b1ce8846f
SparshKhanna0001 <65919066+SparshKhanna0001@users.noreply.github.com>
2021-11-20 23:15:00 +0530

    Format the way data will be dsiplayed

--- si.py
+++ si.py
@@ -10,12 +10,32 @@ type_of_opr = sys.argv[1]
 if type_of_opr.lower() == "all":
   
   for row in cur.execute("SELECT * FROM tasks"):
-    print(f"""(SerialNum = {row[0]}, taskname={row[1]},type={row[2]},Subject={row[3]}, priorityType={row[4]}, description={row[5]},Status={row[6]},DateOfEntry = {row[7]}, weeknum={row[8]},DateCompletion={row[9]},estimatedType = {row[10]})""")
+    #print(f"""(SerialNum = {row[0]}, taskname={row[1]},type={row[2]},Subject={row[3]}, priorityType={row[4]}, description={row[5]},Status={row[6]},DateOfEntry = {row[7]}, weeknum={row[8]},DateCompletion={row[9]},estimatedType = {row[10]})""")
+    print(f'''(SerialNum = {row[0]}''')
+    print(f'''taskname={row[1]}''')
+    print(f'''type={row[2]}''')
+    print(f'''Subject={row[3]}''')
+    print(f'''priorityType={row[4]}''')
+    print(f'''description={row[5]}''')
+    print(f'''Status={row[6]}''')
+    print(f'''DateOfEntry = {row[7]}''')
+    print(f'''weeknum={row[8]}''')
+    print(f'''DateCompletion={row[9]}''')
+    print(f'''estimatedType = {row[10]})''')
+    print('\n')
 
 elif type_of_opr.lower() == "pen":
   
   for row in cur.execute("SELECT * FROM tasks WHERE Status == 'pending'"):
-    print(f"""(SerialNum = {row[0]}, taskname={row[1]},type={row[2]},Subject={row[3]}, priorityType={row[4]}, description={row[5]},Status={row[6]},DateOfEntry = {row[7]}, weeknum={row[8]},DateCompletion={row[9]},estimatedType = {row[10]})""")
+    #print(f"""(SerialNum = {row[0]}, taskname={row[1]},type={row[2]},Subject={row[3]}, priorityType={row[4]}, description={row[5]},Status={row[6]},DateOfEntry = {row[7]}, weeknum={row[8]},DateCompletion={row[9]},estimatedType = {row[10]})""")
+    print(f'''(SerialNum = {row[0]}''')
+    print(f'''taskname={row[1]}''')
+    print(f'''type={row[2]}''')
+    print(f'''Subject={row[3]}''')
+    print(f'''priorityType={row[4]}''')
+    print(f'''description={row[5]}''')
+    print(f'''Status={row[6]}''')
+    print(f'''estimatedType = {row[10]})''')
     
 elif type_of_opr.lower() == "done":
   
